38a3545ac1aed60c8a54e5889ff8dbfe0bf90a96
Petingo <eyanghongbin0315@gmail.com>
2019-11-14 15:41:48 +0800

    update Nov. 14

--- extension/app.js
+++ extension/app.js
@@ -1,49 +1,116 @@
 ; (function () {
+    console.log("1")
 
-    var container;
-
-    var videoAdDelay = 5;
-    var overlayAdDelay = 3;
+    let videoAdDelay = 5;
+    let overlayAdDelay = 3;
+    let nextVideoDelay = 8;
 
     chrome.storage.sync.get({
         videoAdDelay: 5,
-        overlayAdDelay: 3
-    }, function (items) {
-        videoAdDelay = items.videoAdDelay;
-        overlayAdDelay = items.overlayAdDelay;
+        overlayAdDelay: 3,
+        nextVideoDelay: 8
+    }, function (config) {
+        console.log("retrived config:", config)
+        videoAdDelay = config.videoAdDelay;
+        overlayAdDelay = config.overlayAdDelay
+        nextVideoDelay = config.nextVideoDelay
+        console.log(videoAdDelay, overlayAdDelay, nextVideoDelay);
+
+        let setupStatus = {
+            videoPopup: false,
+            videoEnd: false,
+            ad: false
+        }
+    
+        let tmp = setInterval(function () {
+            let playerContainer;
+            if (window.location.pathname !== '/watch') {
+                return;
+            }
+    
+            // video automatically paused in background
+            // let video = document.getElementsByTagName('video')[0]
+            // if(video && !setupStatus.videoEnd){
+            //     setupStatus.videoEnd = true
+            //     video.addEventListener('ended', function (e) {
+            //         let endedVideo = window.location.href
+            //         console.log(window.location.href)
+            //         setTimeout(() => {
+            //             console.log(window.location.href)
+            //             if(window.location.href == endedVideo){
+            //                 console.log("go to up next video")
+            //                 document.querySelector("#dismissable > div.metadata.style-scope.ytd-compact-video-renderer > a").click()
+            //             } else {
+            //                 console.log("already get to next")
+            //             }
+                        
+            //         }, nextVideoDelay * 1000)
+            //     });
+            // }
+            
+    
+            // video has already paused popup
+            popupContainer = document.querySelector("body > ytd-app > ytd-popup-container")
+            if (popupContainer && !setupStatus.videoPopup) {
+                setupStatus.videoPopup = true
+                popupContainer.arrive("paper-dialog > yt-confirm-dialog-renderer .buttons.style-scope.yt-confirm-dialog-renderer #confirm-button paper-button", function () {
+                    document.querySelector("body > ytd-app > ytd-popup-container > paper-dialog > yt-confirm-dialog-renderer .buttons.style-scope.yt-confirm-dialog-renderer #confirm-button paper-button").click()
+                })
+            }
+    
+            // ad skipper
+            playerContainer = document.getElementById('player-container');
+            if (playerContainer && !setupStatus.ad) {
+                setupStatus.ad = true
+    
+                videoAd = document.querySelector("#player-container .ytp-ad-skip-button-container")
+                overlayAd = document.querySelector("#player-container .ytp-ad-overlay-close-button")
+    
+                if (videoAd) {
+                    console.log("close video Ad")
+                    click(videoAd, videoAdDelay * 1000)
+                }
+    
+                if (overlayAd) {
+                    console.log("close overlay Ad")
+                    click(overlayAd, overlayAdDelay * 1000)
+                }
+    
+                playerContainer.arrive(".ytp-ad-skip-button-container", function () {
+                    console.log("close video Ad")
+                    click(this, overlayAdDelay * 1000);
+    
+                })
+                playerContainer.arrive(".ytp-ad-overlay-close-button", function () {
+                    console.log("close overlay Ad")
+                    click(this, videoAdDelay * 1000);
+                })
+    
+            }
+    
+            console.log(setupStatus)
+            let setupStatusResult = true
+            for(let key in setupStatus){
+                setupStatusResult = setupStatusResult && setupStatus[key]
+            }
+            if(setupStatus){
+                clearInterval(tmp);
+            }
+        }, 250);
     });
-
-
-
-    var tmp = setInterval(function () {
-        if (window.location.pathname !== '/watch') {
-            return;
-        }
-        container = document.getElementById('player-container');
-
-        if (container) {
-            container.arrive(".ytp-ad-overlay-close-button", function () {
-                click(this, overlayAdDelay * 1000);
-            })
-            container.arrive(".ytp-ad-skip-button.ytp-button", function () {
-                click(this, videoAdDelay * 1000);
-            })
-            clearInterval(tmp);
-        }
-
-    }, 500);
 
     function click(element, delay) {
         setTimeout(function () {
-            if (element.fireEvent) {
-                element.fireEvent('onclick')
-            } else {
-                let click = document.createEvent('Events');
-                click.initEvent('click', true, false);
-                element.dispatchEvent(click);
-            }
+            // if (element.fireEvent) {
+            //     element.fireEvent('onclick')
+            // } else {
+            //     let click = document.createEvent('Events');
+            //     click.initEvent('click', true, false);
+            //     element.dispatchEvent(click);
+            // }
+            console.log("fired")
+            element.click()
         }, delay);
     }
 
 })();
-
--- extension/options/options.js
+++ extension/options/options.js
@@ -1,25 +1,32 @@
 // save options when "save" button click
-function save_options() {
-    let videoAdDelay = document.getElementById('videoAdDelay').value;
-    let overlayAdDelay = document.getElementById('overlayAdDelay').value;
+function saveOptions() {
+    let videoAdDelay = Number(document.getElementById('videoAdDelay').value)
+    let overlayAdDelay = Number(document.getElementById('overlayAdDelay').value)
 
     var status = document.getElementById('status');
 
     if (!isNaN(videoAdDelay) && !isNaN(overlayAdDelay) && videoAdDelay >= 0 && overlayAdDelay >= 0) {
+        console.log({
+            videoAdDelay: videoAdDelay,
+            overlayAdDelay: overlayAdDelay
+        })
         chrome.storage.sync.set({
             videoAdDelay: videoAdDelay,
             overlayAdDelay: overlayAdDelay
         }, function () {
             // Update status to let user know options were saved.
+            status.classList.add("mt-3")
             status.textContent = 'Saved.';
         });
     } else {
+        status.classList.add("mt-3")
+        // status.style.visibility = "visible";
         status.textContent = 'Invalid input';
     }
 }
 
 // Restores setting using the preferences stored in chrome.storage.
-function restore_options() {
+function restoreOptions() {
     chrome.storage.sync.get({
         videoAdDelay: 5,
         overlayAdDelay: 3
@@ -29,5 +36,5 @@ function restore_options() {
     });
 }
 
-document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click', save_options);
+document.addEventListener('DOMContentLoaded', restoreOptions);
+document.getElementById('save').addEventListener('click', saveOptions);
